658e51b2c16a152df36954ca5c1f27b26c9c31a0
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-13 12:12:31 +0900

    :tada: 초기세팅

---
+++ src/test/kotlin/taekyoung/TodoList/TodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package taekyoung.TodoList
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
