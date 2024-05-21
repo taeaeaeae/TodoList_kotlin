26e434420e807c4c333ccdd9810374b45ae46f45
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-21 16:21:15 +0900

    :sparkles: feat: 댓글 같이 받을 거 새로 생성

---
+++ src/main/kotlin/taekyoung/TodoList/todos/dto/GetTodoResponse.kt
@@ -0,0 +1,10 @@
+package taekyoung.TodoList.todos.dto
+
+data class GetTodoResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val yn : Boolean,
+    val uid: String,
+    val reply: List<ReplyResponse>
+)
