070c973f988f60b00f996c518b917f62088d5207
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-22 11:39:37 +0900

    :sparkles: feat: 데이터 검증

--- src/main/kotlin/taekyoung/TodoList/todos/controller/TodoController.kt
+++ src/main/kotlin/taekyoung/TodoList/todos/controller/TodoController.kt
@@ -3,7 +3,6 @@ package taekyoung.TodoList.todos.controller
 import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
-import org.springframework.data.web.SortDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
