728eb5259cdb5fa2d453b43b5524d1ea9c233d14
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-23 16:44:39 +0900

    :sparkles: feat: security setting 토큰만들고 암호화한거같음

---
+++ src/main/kotlin/taekyoung/TodoList/global/config/JwtSecurityConfig.kt
@@ -0,0 +1,20 @@
+package taekyoung.TodoList.global.config
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+class JwtSecurityConfig(
+    private val tokenProvider: TokenProvider
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(http: HttpSecurity) {
+        // security 로직에 JwtFilter 등록
+        http.addFilterBefore(
+            JwtFilter(tokenProvider),
+            UsernamePasswordAuthenticationFilter::class.java
+        )
+    }
+}
--- src/main/kotlin/taekyoung/TodoList/global/config/TokenProvider.kt
+++ src/main/kotlin/taekyoung/TodoList/global/config/TokenProvider.kt
@@ -1,6 +1,6 @@
 package taekyoung.TodoList.global.config
 
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm
+import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.*
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
@@ -13,69 +13,67 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
+import taekyoung.TodoList.user.model.User
+import java.nio.charset.StandardCharsets
 import java.security.Key
 import java.util.*
 import java.util.stream.Collectors
+import javax.crypto.SecretKey
 
 @Component
 class TokenProvider (
-    private val logger: Logger = LoggerFactory.getLogger(TokenProvider::class.java),
-    private val AUTHORITIES_KEY: String = "auth",
+    @Value("\${JWT_SECRET}")
+    private var secret: String, // = null,
 
-    @Value("\${jwt.secret}")
-    private var secret: String? = null,
+    @Value("\${JWT_ACCESS}")
+    private var tokenValidityInMilliseconds: Long,
 
-    @Value("\${jwt.token-validity-in-seconds}")
-    private var tokenValidityInMilliseconds: Long = 0,
-
-    private var key: Key? = null
 ) : InitializingBean {
-
-//    tokenValidityInMilliseconds = tokenValidityInSeconds * 1000
+    private val logger: Logger = LoggerFactory.getLogger(TokenProvider::class.java)
+    private val AUTHORITIES_KEY: String = "auth"
+    private var key: SecretKey? = null
 
     override fun afterPropertiesSet() {
-        val keyBytes: ByteArray = Decoders.BASE64.decode(secret)
-        this.key = Keys.hmacShaKeyFor(keyBytes)
+        val keyBytes: ByteArray =secret.toByteArray(StandardCharsets.UTF_8)
+        key = Keys.hmacShaKeyFor(keyBytes)
     }
 
-//    fun createToken(authentication: Authentication): String {
-//    tokenValidityInMilliseconds = tokenValidityInSeconds * 1000
-//        val authorities: String = authentication.getAuthorities().stream()
-//            .map { obj: GrantedAuthority -> obj.authority }
-//            .collect(Collectors.joining(","))
-//
-//        // 토큰의 expire 시간을 설정
-//        val now: Long = Date().getTime()
-//        val validity: Date = Date(now + this.tokenValidityInMilliseconds)
-//
-//        return Jwts.builder()
-//            .setSubject(authentication.getName())
-//            .claim(AUTHORITIES_KEY, authorities) // 정보 저장
-//            .signWith(key, SignatureAlgorithm.HS512) // 사용할 암호화 알고리즘과 , signature 에 들어갈 secret값 세팅
-//            .setExpiration(validity) // set Expire Time 해당 옵션 안넣으면 expire안함
-//            .compact()
-//    }
+    fun createToken(authentication: Authentication): String {
+//        tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000
+        val authorities: String = authentication.getAuthorities().stream()
+            .map { obj: GrantedAuthority -> obj.authority }
+            .collect(Collectors.joining(","))
+
+        // 토큰의 expire 시간을 설정
+        val now: Long = Date().getTime()
+        val validity: Date = Date(now + this.tokenValidityInMilliseconds)
+
+        return Jwts.builder()
+            .setSubject(authentication.getName())
+            .claim(AUTHORITIES_KEY, authorities) // 정보 저장
+            .signWith(key, SignatureAlgorithm.HS512) // 사용할 암호화 알고리즘과 , signature 에 들어갈 secret값 세팅
+            .setExpiration(validity) // set Expire Time 해당 옵션 안넣으면 expire안함
+            .compact()
+    }
 
     // 토큰으로 클레임을 만들고 이를 이용해 유저 객체를 만들어서 최종적으로 authentication 객체를 리턴
     fun getAuthentication(token: String): Authentication {
-        TODO()
-//        val claims: Claims = Jwts
-//            .parserBuilder()
-//            .setSigningKey(key)
-//            .build()
-//            .parseClaimsJws(token)
-//            .getBody()
+//        TODO()
+        val claims: Claims = Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .body
 
-//        val authorities: Collection<GrantedAuthority> =
+        val authorities: Collection<GrantedAuthority> = ArrayList()
 //            Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
 //                .map { role: String? -> SimpleGrantedAuthority(role) }
 //                .collect(Collectors.toList())
-//
-//        val principal: User = User(claims.getSubject(), "", authorities)
-//
-//
-//
-//        return UsernamePasswordAuthenticationToken(principal, token, authorities)
+
+        val principal: User = User(claims.getSubject(), "")
+
+        return UsernamePasswordAuthenticationToken(principal, token, authorities)
     }
 
     // 토큰의 유효성 검증을 수행
