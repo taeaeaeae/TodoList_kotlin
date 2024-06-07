34f84963884b7fd9ebacd36bed3fab3cb648b972
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-07 11:57:37 +0900

    :truck: rename: 도메인패키지로 변경

--- src/main/kotlin/taekyoung/TodoList/domain/todos/service/TodoService.kt
+++ src/main/kotlin/taekyoung/TodoList/domain/todos/service/TodoService.kt
@@ -2,11 +2,7 @@ package taekyoung.TodoList.domain.todos.service
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import taekyoung.TodoList.domain.todos.dto.CreateTodoRequest
-import taekyoung.TodoList.domain.todos.dto.GetTodoResponse
-import taekyoung.TodoList.domain.todos.dto.TodoResponse
-import taekyoung.TodoList.domain.todos.dto.UpdateTodoRequest
-import taekyoung.TodoList.todos.dto.*
+import taekyoung.TodoList.domain.todos.dto.*
 
 interface TodoService {
     fun getAllTodo(pageable: Pageable, uid:String?) : Page<TodoResponse>
